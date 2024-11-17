ae4c029275b9c4c05f4270711fdd30aa2c266f02
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2024-11-17 20:50:46 +0700

    first commit

---
+++ road-map-node.js
@@ -0,0 +1,88 @@
+const roadMap = {
+   MySQL: {
+      "Buổi 1": "Khải giảng",
+      "Buổi 2": "Basic SQL",
+      "Buổi 3": "Truy vấn SQL, Relationship",
+      "Buổi 4": "Phân tích db db_cyber_media"
+   },
+   ExpressJS: {
+      "Buổi 5": [
+         "- Làm quen với expressjs",
+         "- MySql2, Kết nối DB",
+         "- Kết nối FE, BE, DB",
+         "- CORS"
+      ],
+      "Buổi 6": [
+         "- ORM sequelize",
+         "- Code first & Database first",
+         "- Kiểm tra kết nối db",
+         "- ORM Prisma"
+      ],
+      "Buổi 7": [
+         "- Tìm hiểu mô hình MVC",
+         "- Xử lý lỗi"
+      ],
+   },
+   Auth: {
+      "Buổi 8": [
+         "- Xây dựng API Đăng ký & Đăng nhập",
+         "- Mã hoá password",
+         "- Login Facebook, Demo hack FB"
+      ],
+      "Buổi 9": [
+         "- Gửi email",
+         "- Cấu trúc token",
+         "- Access Token (jsonwebtoken)"
+      ],
+      "Buổi 10": [
+         "- Refresh Token (jsonwebtoken)",
+         "- Middleware protect bảo vệ API",
+      ],
+      "Buổi 11": [
+         "- Phân quyền",
+         "- Role, Permission"
+      ],
+      "Buổi 12": [
+         "- Viết Unit Test với Jest",
+         "- Debug trên vscode",
+         "- Swagger Open API"
+      ]
+   },
+   Prisma: {
+      "Buổi 13": [
+         "- Xây dựng api get-info",
+         "- Cấu trúc Log terminal, set up color (chalk)",
+         "- Xây dựng chức năng Two FA"
+      ],
+      "Buổi 14": [
+         "- Graphql",
+         "- Upload avatar (image) - Local/Cloud",
+         "- Ráp FE"
+      ],
+      "Buổi 15": [
+         "- Socket.IO",
+         "- Xây dựng API chat"
+      ]
+   },
+   NestJS: {
+      "Buổi 16": [
+         "- Cấu trúc module",
+         "- Upload image",
+         "- Swagger",
+         "- Biến môi trường .env"
+      ],
+      "Buổi 17:": [
+         "- Xử lý Token với passport, stategy"
+      ]
+   },
+   Docker: {
+      "Buổi 18": [
+         "- Tìm hiểu Docker",
+         "- Tìm hiểu Docker Compose"
+      ],
+      "Buổi 19": [
+         "- Deploy source lên server",
+         "- CI/CD"
+      ]
+   }
+}

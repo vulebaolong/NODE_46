fe945247b50e3e0486a684130dcdf1a85b15d8b3
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-01-12 12:09:09 +0700

    buoi 14

--- ExpressJS/be-cyber-media/server.js
+++ ExpressJS/be-cyber-media/server.js
@@ -2,25 +2,25 @@ import express from "express";
 import rootRouter from "./src/routes/root.router.js";
 import { responseError } from "./src/common/helpers/reposonse.helper.js";
 import { handleError } from "./src/common/helpers/error.helper.js";
-import cors from 'cors'
+import cors from "cors";
 
 const app = express();
 
 // middleware giúp phân giải dữ liệu từ json sang đối tượng javascript
 app.use(express.json());
-app.use(cors({
-   origin: ['http://localhost:5173', 'google.com']
-}))
+app.use(
+   cors({
+      origin: ["http://localhost:5173", "google.com"],
+   })
+);
 
 app.use(rootRouter);
 
 app.use(handleError);
 
-
 app.listen(3069, () => {
    console.log(`Server Online At Port 3069`);
 });
-
 
 /**
  * tự động lưu token ở POSTMAN
@@ -137,3 +137,115 @@ pm.globals.set("refreshToken", refreshToken);
 //    console.log(request.headers);
 //    response.json(`Header Parameters`);
 // });
+
+/*
+
+    // BE - ExpressJS CURD
+    // Routes
+    "Express CRUD Routes": {
+      "scope": "javascript, javascriptreact, typescriptreact, typescript",
+      "prefix": "express-router-crud",
+      "body": [
+          "import express from 'express';",
+          "",
+          "const ${1:module}Router = express.Router();",
+          "",
+          "// Tạo route CRUD",
+          "${1:module}Router.post('/', ${1:module}Controller.create);",
+          "${1:module}Router.get('/', ${1:module}Controller.findAll);",
+          "${1:module}Router.get('/:id', ${1:module}Controller.findOne);",
+          "${1:module}Router.patch('/:id', ${1:module}Controller.update);",
+          "${1:module}Router.delete('/:id', ${1:module}Controller.remove);",
+          "",
+          "export default ${1:module}Router;"
+      ],
+      "description": "Snippet for Express CRUD routes"
+  },
+  "Express CRUD Controller": {
+      "scope": "javascript, javascriptreact, typescriptreact, typescript",
+      "prefix": "express-controller-crud",
+      "body": [
+          "export const ${1:module}Controller = {",
+          "   create: async function (req, res, next) {",
+          "      try {",
+          "         const result = await ${2:${1:module}Service}.create(req);",
+          "         const response = ${3:responseSuccess}(result, `Create ${1:module} successfully`);",
+          "         res.status(response.code).json(response);",
+          "      } catch (err) {",
+          "         next(err);",
+          "      }",
+          "   },",
+          "",
+          "   findAll: async function (req, res, next) {",
+          "      try {",
+          "         const result = await ${1:module}Service.findAll(req);",
+          "         const response = responseSuccess(result, `Get all ${1:module}s successfully`);",
+          "         res.status(response.code).json(response);",
+          "      } catch (err) {",
+          "         next(err);",
+          "      }",
+          "   },",
+          "",
+          "   findOne: async function (req, res, next) {",
+          "      try {",
+          "         const result = await ${1:module}Service.findOne(req);",
+          "         const response = responseSuccess(result, `Get ${1:module} #${req.params.id} successfully`);",
+          "         res.status(response.code).json(response);",
+          "      } catch (err) {",
+          "         next(err);",
+          "      }",
+          "   },",
+          "",
+          "   update: async function (req, res, next) {",
+          "      try {",
+          "         const result = await ${1:module}Service.update(req);",
+          "         const response = responseSuccess(result, `Update ${1:module} #${req.params.id} successfully`);",
+          "         res.status(response.code).json(response);",
+          "      } catch (err) {",
+          "         next(err);",
+          "      }",
+          "   },",
+          "",
+          "   remove: async function (req, res, next) {",
+          "      try {",
+          "         const result = await ${1:module}Service.remove(req);",
+          "         const response = responseSuccess(result, `Remove ${1:module} #${req.params.id} successfully`);",
+          "         res.status(response.code).json(response);",
+          "      } catch (err) {",
+          "         next(err);",
+          "      }",
+          "   }",
+          "};"
+      ],
+      "description": "Snippet for Express CRUD controller"
+  },
+  "Express CRUD Service": {
+      "scope": "javascript, javascriptreact, typescriptreact, typescript",
+      "prefix": "express-service-crud",
+      "body": [
+          "export const ${1:module}Service = {",
+          "   create: async function (req) {",
+          "      return `This action create`;",
+          "   },",
+          "",
+          "   findAll: async function (req) {",
+          "      return `This action returns all ${1:module}`;",
+          "   },",
+          "",
+          "   findOne: async function (req) {",
+          "      return `This action returns a id: ${req.params.id} ${1:module}`;",
+          "   },",
+          "",
+          "   update: async function (req) {",
+          "      return `This action updates a id: ${req.params.id} ${1:module}`;",
+          "   },",
+          "",
+          "   remove: async function (req) {",
+          "      return `This action removes a id: ${req.params.id} ${1:module}`;",
+          "   },",
+          "};"
+      ],
+      "description": "Snippet for Express CRUD service"
+  }
+
+  */
--- ExpressJS/be-cyber-media/src/routes/root.router.js
+++ ExpressJS/be-cyber-media/src/routes/root.router.js
@@ -2,6 +2,8 @@ import express from 'express'
 import videoRouter from './video.router.js'
 import carRouter from './car.router.js';
 import authRouter from './auth.router.js';
+import roleRouter from './role.router.js';
+import permissionRouter from './permission.router.js';
 
 const rootRouter = express.Router()
 
@@ -13,5 +15,7 @@ rootRouter.get(`/`, (request, response, next) => {
 rootRouter.use('/video', videoRouter)
 rootRouter.use(`/car`, carRouter)
 rootRouter.use(`/auth`, authRouter)
+rootRouter.use(`/role`, roleRouter)
+rootRouter.use(`/permission`, permissionRouter)
 
 export default rootRouter
